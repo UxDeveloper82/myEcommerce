0622ad5935163440acf07b64dc401e0f46ade954
UxDeveloper82 <electronicsmf@aol.com>
2019-12-05 21:53:22 -0500

    creating the table area

--- js/contact.js
+++ js/contact.js
@@ -8,11 +8,49 @@ class Message {
 
 
 class UI {
+   static displayMessages() {
 
+       const StoredMessages = [
+           {
+               name: 'Martin Fernandez',
+               email: 'Admin@gmail.com',
+               message: 'Need a quote for Application'
+           },
+           {
+            name: 'Jerry Springer',
+            email: 'Jerry@gmail.com',
+            message: 'Need a quote for Application'
+           }, 
+           {
+            name: 'Robert Falkon',
+            email: 'Falkon@gmail.com',
+            message: 'Need a quote for Application'
+        }
+       ];
 
+       const myMessages = StoredMessages;
+         
+       myMessages.forEach((mymessage) => UI.addMessageToList(mymessage));
+   }
+ 
+    static addMessageToList(mymessage) {
+        const list = document.querySelector('#message-list')
+        const row = document.createElement('tr');
+        row.innerHTML = `
+           <td>${mymessage.name}</td>
+           <td>${mymessage.email}</td> 
+           <td>${mymessage.message}</td>
+           <td><a href="#" class="btn btn-danger btn-sm 
+           delete">X</a></td> 
+        `;
+        list.appendChild(row);
+    }
 
-
-
+  static clearFields() {
+      document.querySelector('#name').value = '';
+      document.querySelector('#email').value = '';
+      document.querySelector('#message').value = '';
+  }
 }
 
 
@@ -25,6 +63,14 @@ document.querySelector('#form-area').addEventListener('submit', (e) => {
      const name = document.querySelector('#name').value;
      const email = document.querySelector('#email').value;
      const message = document.querySelector('#message').value;
-     console.log(name);
-}) 
+
+     const mymessage = new Message(name,email,message);
+     console.log(mymessage);
+
+     UI.clearFields();
+     
+});
+
+document.addEventListener('DOMContentLoaded', UI.displayMessages);
+
 
