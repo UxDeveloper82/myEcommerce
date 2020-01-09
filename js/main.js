fc9bc432d97e1dee516b4d737ac0b16676f63c78
UxDeveloper82 <electronicsmf@aol.com>
2020-01-08 22:14:33 -0500

    slide

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const current = document.querySelector("#current");
+const imgs = document.querySelectorAll(".imgs img");
+
+imgs.forEach(img => img.addEventListener('click', (e) => 
+current.src = e.target.src));
+
+
 $(document).ready(function () {
 
 
